c5789c7244612bec12e56205f8a444302ccd7546
TEKELI Emin-Can <emincan.tekeli@outlook.fr>
2021-02-10 16:55:29 +0100

    Salut Enes

--- Voronoi.h
+++ Voronoi.h
@@ -6,7 +6,8 @@
 #include <vector>
 
 #include "Sommet.h"
-#include "Face.h"
+#include "Triangle.h"
+
 
 using namespace std;
 
@@ -20,9 +21,59 @@ template <class S, class T>
 class Voronoi{
 public:
 	vector<Sommet<T>> sommets;//Sommets en entrée
-	vector<Face<S, T>> faces;//Face en sortie
+	vector<Triangle<S, T>> triangles;//Triangles en sortie
+	vector<Triangle<S, T>> DTL;//Triangles à supprimer de la triangulation
+	vector<Triangle<S, T>> NTL;//Triangles à rajouter à la triangulation
+
 
 	Voronoi(vector<Sommet<T>> sommets):sommets(sommets){}
 	~Voronoi(){}
-	void triangulate();
+
+	// Triangule la liste de sommets en entrée
+	void triangulate() {
+		determiner_triangulation_englobante();
+		for (Sommet s : sommets){
+			determiner_DTL(s, t);
+			determiner_NTL(s);
+			supprimer_DTL();
+		}
+	}
+
+	// Détermine une triangulation qui englobe tous les sommets à trianguler
+	void determiner_triangulation_englobante() {
+		int xMin = sommets.front().getX();
+		int xMax = sommets.front().getX();
+		int yMin = sommets.front().getY();
+		int yMax = sommets.front().getY();
+		for (Sommet s : sommets) {
+			(s.getX() < xMin) ? xMin = s.getX();
+			(s.getY() < yMin) ? yMin = s.getY();
+			(s.getX() > xMax) ? xMax = s.getX();
+			(s.getY() > yMax) ? yMax = s.getY();
+		}
+	}
+
+	// Détermine la liste des triangles à supprimer de la triangulation
+	void determiner_DTL(Sommet s, Triangle t) {
+
+	}
+
+	// Détermine la liste des triangles à rajouter à la triangulation
+	void determiner_NTL(Sommet s) {
+
+	}
+	
+	// Supprime les triangles en commun dans DTL et Triangles
+	void supprimer_DTL() {
+		for (i = DTL.begin(); i != DTL.end(); i++) {
+			for (j = Triangles.begin(); j != Triangles.end(); j++) {
+				if (*i == *j) {
+					DTL.erase(i);
+					Triangles.erase(j);
+					break;
+				}
+			}
+		}
+	}
+
 };
