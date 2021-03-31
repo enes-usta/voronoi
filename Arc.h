d4ef44ab3d59d089856d721bfd50135116e49f6b
TEKELI Emin-Can <emincan.tekeli@outlook.fr>
2021-03-31 15:05:29 +0200

    corrections de plus de bugs pourris

--- Arc.h
+++ Arc.h
@@ -10,7 +10,7 @@ class Arc {
 public:
 	Arete<S, T> *arete;
 	int sens;
-	public Arc(Arete<S, T> *arete, int sens): arete(arete), sens(sens){}
+	public Arc(Arete<S, T>* arete, int sens): arete(arete), sens(sens){}
 
 	/**
 	* Retourne vrai si le sommet s est à gauche de cet arc
@@ -18,9 +18,9 @@ public:
 	bool estAGauche(Sommet<T> s) {
 		Sommet<T> a = this.arete->debut.v;
 		Sommet<T> b = this.arete->fin.v;
-		if (this->arete.sens == 1) {
-			a = this->arete.fin.v;
-			b = this->arete.debut.v;
+		if (this.sens == 1) {
+			a = this.arete->fin.v;
+			b = this.arete->debut.v;
 		}
 
 		return (b.x - a.x) * (s.y - a.y) - (s.x - a.x) * (b.y - a.y);
