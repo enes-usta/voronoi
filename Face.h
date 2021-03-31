6cde2740082688ff377aea83702425ad82f84e59
TEKELI Emin-Can <emincan.tekeli@outlook.fr>
2021-03-31 21:11:41 +0200

    plus d'erreurs, à voir maitnenant si les algos fonctionnent

--- Face.h
+++ Face.h
@@ -9,21 +9,21 @@ template <class S>
 class Face{
 public:
 	vector<Arc<S>> arcs;
-	Face(vector<Arc<S>> &arcs) {
+	Face(vector<Arc<S>> arcs) {
 		this->arcs = arcs;
-		if (this->arcs.size < 3)
+		if (this->arcs.size() < 3)
 			throw "Une face est composée d'au moins 3 arcs!";
 	}
 
 	/**
 	* Retourne vrai si cette face contient le sommet s
 	*/
-	/*bool contientPoint(Sommet<Vecteur2D> s) {
+	bool contientPoint(Sommet<Vecteur2D> s) {
 		for each (Arc<S> arc in this->arcs)
-			if (!arc.estAGauche(s))
+			if (arc.estAGauche(s))
 				return false;
-
+				
 		return true;
-	}*/
+	}
 
 };
